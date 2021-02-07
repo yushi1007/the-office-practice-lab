04a64f1138ad1c05518029d8c54eea9115602159
Sandra Wazaz <sandrawazaz@gmail.com>
2021-02-07 15:12:41 -0500

    sandra edit

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,7 @@ class Employee
     
     @@all = []
 
-    attr_accessor :name, :salary
+    attr_accessor :name, :salary, :employees
 
     def initialize(name, salary)
         @name = name
@@ -13,5 +13,18 @@ class Employee
     def self.all
         @@all
     end
+
+        #1. pass argumet of employees name 
+        #2. itirate over all roles and return that employees   roles instances 
+        # returns an instance of their manager
+        
+    def self.employees(name)
+        Role.all.select{|role| role.employee.name == name}
+    end 
+
+
+    # def manager 
+    #     self.employees.map
+    # end 
 
 end
