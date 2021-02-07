91fe27799632d841ed781b23f2eb6e7debc2a8d7
Yu Shi <yushiys95@gmail.com>
2021-02-07 12:44:42 -0500

    yu edited

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../config/environment.rb'
 
 def reload
@@ -9,18 +10,20 @@ end
 ben = Employee.new("Ben", 25000) # one role, one manager
 lisa = Employee.new("Lisa", 100000) # two roles, two managers
 mark = Employee.new("Mark", 50000) # one role, one manager
-sally = Employee.new("Sally", 60000) # 
+sally = Employee.new("Sally", 60000) # no role
 
 yu = Manager.new("Yu", "Accounting", 35) # managing one
-ted = Manager.new("Ted", "Tech", 45) # managing one
+ted = Manager.new("Ted", "Tech", 45) # managing two
 bob = Manager.new("Bob", "HR", 22) #managing none
 sandra = Manager.new("Sandra", "Design", 56) #managing two
 
-project_manager = Role.new("Project Manager", ben, yu)
-accounts_director = Role.new("Accounts Director", lisa, ted)
+project_manager = Role.new("Project Manager", ben, yu) 
+accounts_director = Role.new("Accounts Director", lisa, ted) 
 designer = Role.new("Designer", lisa, ted)
 copywriter = Role.new("Copywriter", mark, sandra)
 coffeemaker = Role.new("Coffee Maker", ben, sandra)
 
+project_manager.employees
+ben.manager
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
