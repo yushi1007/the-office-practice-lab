3e8d5aff3b090825d5647644dd1ce770b125b7f4
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-07 11:44:57 -0500

    changes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,17 @@
 class Employee
+    
+    @@all = []
+
+    attr_accessor :name, :salary
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,3 +1,18 @@
 class Manager
+    
+    @@all = []
+
+    attr_accessor :name, :department, :age
+
+    def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+    end
+
+    def self.all
+        @@all    
+    end
 
 end
