3a3e5b03595d248e00d5a85e2310d418c49a81b0
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-07 19:55:23 -0500

    yehudis edits

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -31,17 +31,28 @@ class Employee
         # binding.pry
         self.all.select{|employee| employee.salary >= salary} 
     end
-    def self.find_by_department(department)
-      # Manager.all.find{|manager| manager.department == "Design"}
-        binding.pry
-    end
+
+    # def self.find_by_department(department)
+    #   # Manager.all.find{|manager| manager.department == "Design"}
+    # # Role.all.find{|role| role.manager.department == "Design"}  
+    #   binding.pry
+    # end
+
+    # def self.search_by_role(role_name)
+    #     binding.pry
+    # end
 end
 
 
 
 # Employee.find_by_department
-# takes a String argument that is the name of a department and returns the first employee whose manager is working in that department
+# takes a String argument that is the name of a department 
+# and returns the first employee whose manager is working in that department 
+#1. iterate over managers to match department
+#2. return managers employee
+
 # Employee.search_by_role
 # takes a String argument that is the name of a role and returns all the employees who work at that role
+
 # Employee#get_promoted
 # takes an argument of an instance of a Role, updates Employee's role and increases Employee's salary.​
